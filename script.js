e0d139b7b31cc79625c4ebc12f0f58d57988784c
didiwot <didiwot@yandex.ru>
2022-02-26 12:25:36 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let submitButton = document.getElementById("button");
+let textOutput = document.getElementById("output");
+
+submitButton.addEventListener("click", async function(event) {
+    event.preventDefault();
+    try {
+        let response = await fetch("http://localhost:3000/auth/hello");
+        let data = await response.json();
+        textOutput.innerText = data;
+    } catch (error) {
+        console.log(error)
+    }
+})
